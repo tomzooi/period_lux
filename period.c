20a47586fae975d93d8918db9b808fd66d753c14
Tom <tom@berg-vanden.nl>
2015-02-03 16:55:37 +0100

    B

--- period.c
+++ period.c
@@ -141,14 +141,18 @@ void everysecond() {
 }
 
  int getLux(int fd){
-       wiringPiI2CWriteReg8(fd, TSL2561_COMMAND_BIT, TSL2561_CONTROL_POWERON); //enable the device
-       wiringPiI2CWriteReg8(fd, TSL2561_REGISTER_TIMING, TSL2561_GAIN_AUTO); //auto gain and timing = 101 mSec
-       //Wait for the conversion to complete
-       delay(LUXDELAY);
-       //Reads visible + IR diode from the I2C device auto
-       uint16_t visible_and_ir = wiringPiI2CReadReg16(fd, TSL2561_REGISTER_CHAN0_LOW);
-       wiringPiI2CWriteReg8(fd, TSL2561_COMMAND_BIT, TSL2561_CONTROL_POWEROFF); //disable the device
-       return visible_and_ir*2;
+       if (wiringPiI2CWriteReg8(fd, TSL2561_COMMAND_BIT, TSL2561_CONTROL_POWERON) >= 0) { //enable the device
+		   wiringPiI2CWriteReg8(fd, TSL2561_REGISTER_TIMING, TSL2561_GAIN_AUTO); //auto gain and timing = 101 mSec
+		   //Wait for the conversion to complete
+		   delay(LUXDELAY);
+		   //Reads visible + IR diode from the I2C device auto
+		   uint16_t visible_and_ir = wiringPiI2CReadReg16(fd, TSL2561_REGISTER_CHAN0_LOW);
+		   wiringPiI2CWriteReg8(fd, TSL2561_COMMAND_BIT, TSL2561_CONTROL_POWEROFF); //disable the device
+		   return visible_and_ir*2;
+       }
+       else {
+		   printf("i2c poep");
+	   }
     }
 
 
